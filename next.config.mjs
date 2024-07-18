f06d9b632dec773952423239a00ef51edd85202a
freaky-godlike <145795462+freaky-godlike@users.noreply.github.com>
2024-07-18 18:38:14 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            "kknobcejplpipiyhdaqu.supabase.co"
+        ]
+    }
+};
+
+export default nextConfig;
